5847f7c5ffc3cd69e5a4b3250ec9c13b00404e7e
Patrick Richardson <luckysob86@gmail.com>
2016-08-01 15:39:52 -0500

    added additional functions

--- pat_lib.js
+++ pat_lib.js
@@ -9,3 +9,31 @@ if(typeof Object.create !== 'function'){
     return new F();
   };
 }
+
+//create method available to all functions. No longer have to type prototype property
+Function.prototype.method = function(name,func){
+  if(!this.prototype[name]){
+    this.prototype[name] = func;
+    return this;
+  }
+};
+
+//creates a closure that holds original function and arguments to curry
+Function.method('curry', function() {
+  var slice = Array.prototype.slice,
+      args = slice.apply(arguments), 
+      that = this;
+  return function() {
+    return that.apply(null, args.concat(slice.apply(arguments)));
+  };
+});
+
+
+//tail recursive function for factorials 
+var factorial = function factorial(i,a){
+  a = a || 1;
+  if (i < 2){
+    return a;
+  }
+  return factorial(i - 1, a * i);
+};
